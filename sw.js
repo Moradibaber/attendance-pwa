433779c4c80697345264e2acb3ca0a8cf4e19297
Moradibaber <mmoradibabersad@gmail.com>
2026-06-22 15:31:28 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "attendance-pwa-v1";
+const CACHE_NAME = "attendance-pwa-v2";
 const FILES = ["./", "index.html", "styles.css", "app.js", "manifest.json"];
 
 self.addEventListener("install", (event) => {

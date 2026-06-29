4b739a414d508d08871fd5cc4f4014881908204b
Moradibaber <mmoradibabersad@gmail.com>
2026-06-29 11:01:41 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "attendance-pwa-v16";
+const CACHE_NAME = "attendance-pwa-v17";
 const FILES = ["./", "index.html", "styles.css", "app.js", "manifest.json"];
 
 const DB_NAME = "attendance-pwa-db";

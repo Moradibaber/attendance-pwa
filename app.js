18ca381ca7aea003f43ea8542b4f9c8f2600eb05
Moradibaber <mmoradibabersad@gmail.com>
2026-06-30 11:30:46 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const DB_NAME = "attendance-pwa-db";
-const DB_VERSION = 4;
+const DB_VERSION = 3;
 const STORE_RECORDS = "records";
 const STORE_PROFILE = "profile";
 const STORE_CONFIG = "config";

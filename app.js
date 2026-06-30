9c9c6ebdd82544b9c9f34a0d639cb9b7a1ad2226
Moradibaber <mmoradibabersad@gmail.com>
2026-06-30 09:45:23 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1104,9 +1104,9 @@ async function syncPendingRecords() {
 
         const res = await fetch(APPS_SCRIPT_URL, {
           method: "POST",
-          headers: {
-            "Content-Type": "text/plain;charset=utf-8"
-          },
+          headers:{
+  "Content-Type":"application/json"
+},
           body: JSON.stringify(payload)
         });
 

76efdc95a82add84954563869151d7fabba461df
Moradibaber <mmoradibabersad@gmail.com>
2026-08-19 21:32:24 +0330

    Update app.js

--- app.js
+++ app.js
@@ -2315,7 +2315,7 @@ function hasStrongGlare_(dataUrl) {
 
         const ratio = veryBright / total;
         console.log("Glare ratio:", (ratio * 100).toFixed(1) + "%");
-      resolve(ratio > 0.004);
+      resolve(ratio > 0.006);
       } catch (e) {
         resolve(false);
       }

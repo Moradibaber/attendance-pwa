8bc5bf887bd735ea4506af14a6f878160e06768d
Moradibaber <mmoradibabersad@gmail.com>
2026-08-19 07:11:33 +0330

    Update app.js

--- app.js
+++ app.js
@@ -2774,7 +2774,7 @@ async function processCapturedPhoto(file) {
             Math.sqrt(Math.pow(cx / w - 0.5, 2) + Math.pow(cy / h - 0.5, 2));
 
           // face must cover enough of the photo and be near center
-          if (faceArea < 0.12 || dist > 0.22) {
+         if (faceArea < 0.16 || dist > 0.18) {
             setBusy(false);
             setStatus(
               "صورت باید بزرگ‌تر و نزدیک وسط کادر باشد.\nدوباره عکس بگیرید."
